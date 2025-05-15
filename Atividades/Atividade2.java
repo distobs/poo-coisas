291aaac6723efe6a5f70d7ca1ee3a2a63915aca3
Distobs <95232743+distobs@users.noreply.github.com>
2025-05-15 14:19:14 -0300

    PRIMEIRJEIORJOKDFLKD OIjk foi
    OIAJSDCONADOFIJASPDOIHNOFDEORSZHNDSCFVHNA\PÇDIOCFVODLIFCVAÇOSDERFCV FOIJCFVOIDCFRVOALIDERSJFHP9Q834RT0F8C9C43-2J-09R8J23-09CFR8-2984CV0-90FJJV4V349NV3490EWEWN TECNOLOGIA AVANCAA JAVA POOOOOOOOOOOOPOOOOOOOR. DXZ NE AAAAAA

---
+++ Atividades/Atividade2.java
@@ -0,0 +1,31 @@
+/* Escreva um programa que pergunte ao usuário quantas notas deseja digitar.
+ * Receba o valor das notas, armazene em um Array.
+ * Depois faça a média das notas e mostre na tela o resultado. */
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Quantas notas?");
+
+		int n = s.nextInt();
+
+		double[] notas = new double[n];
+
+		double media = 0;
+
+		for (int i = 0; i < n; ++i) {
+			notas[i] = s.nextDouble();
+		}
+
+		for (int i = 0; i < n; ++i) {
+			media += notas[i];
+		}
+
+        	media /= 2;
+
+		System.out.println("A média final foi: " + media);
+	}
+}
